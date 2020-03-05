12f054980a5f7dde5b2c875bf15a9ac11601488f
Eric BREHAULT <ebrehault@gmail.com>
2020-03-05 14:23:51 +0100

    Add  option

--- src/command.js
+++ src/command.js
@@ -11,7 +11,8 @@ const configFile = configFileArg && configFileArg.split('=')[1];
 const noConfig = args.indexOf('--no-config') > -1;
 const https = args.indexOf('--https') > -1;
 const defaultToMaster = args.indexOf('--default-to-master') > -1;
+const allMaster = args.indexOf('--all-master') > -1;
 const outputArg = args.find(function(arg) {return arg.startsWith('--output')});
 const output = outputArg && outputArg.split('=')[1];
 
-develop({noFetch, configFile, noConfig, output, reset, lastTag, https, defaultToMaster});
+develop({noFetch, configFile, noConfig, output, reset, lastTag, https, defaultToMaster, allmMster});
